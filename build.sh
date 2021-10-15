ea12583210911b969b4089a5d1b4689a7fbdb662
Will Ryan <will.ryan@gluwa.com>
2021-10-15 16:48:46 -0700

    Use dockerfile only; update CI - Updated implementation to just use Dockerfile (no build.sh) - Updated github actions

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dotnet build --configuration Release
-rm -rf ccgateway/bin/Release/netcoreapp3.1/plugins
-mkdir ccgateway/bin/Release/netcoreapp3.1/plugins
-cp ccgateway/plugins/*.dll ccgateway/bin/Release/netcoreapp3.1/plugins/
-docker build --tag creditcoin-gateway ccgateway/bin/Release/netcoreapp3.1/.
