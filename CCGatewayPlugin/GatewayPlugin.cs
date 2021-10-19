49f551c3da99aed0114a89fbfd11f58dfea29ae3
Nathan Whitaker <nathan.whitaker01@gmail.com>
2021-10-19 18:13:35 -0400

    Add async version of the gateway plugin interface

---
+++ CCGatewayPlugin/GatewayPlugin.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace CCGatewayPlugin
+{
+    public interface ICCGatewayPluginAsync
+    {
+        Task<Tuple<bool, string>> Run(IConfiguration cfg, string[] command);
+    }
+}
